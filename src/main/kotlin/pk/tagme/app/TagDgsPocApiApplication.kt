15b74c7ebb897959b8271ed8d51c8814ecab0167
Vladimir Solovyev <vladimirs@tagme.pk>
2022-09-15 14:40:22 +0300

    spring initializr

---
+++ src/main/kotlin/pk/tagme/app/TagDgsPocApiApplication.kt
@@ -0,0 +1,11 @@
+package pk.tagme.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TagDgsPocApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TagDgsPocApiApplication>(*args)
+}
---
+++ src/test/kotlin/pk/tagme/app/TagDgsPocApiApplicationTests.kt
@@ -0,0 +1,12 @@
+package pk.tagme.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TagDgsPocApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
